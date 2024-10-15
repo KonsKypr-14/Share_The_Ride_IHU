cd87e3c17bdbca5a1b8dd68ed0bfc6586cbc4620
Konstantinos Kyprianidis <kyprianidis.k@outlook.com>
2024-10-15 23:04:48 +0300

    Share the Ride project

--- app/src/main/java/com/example/sharetheride/LoginFragment.java
+++ app/src/main/java/com/example/sharetheride/LoginFragment.java
@@ -106,9 +106,6 @@ public class LoginFragment extends Fragment {
       }
     });
 
-
-
-
     buttonLog.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
